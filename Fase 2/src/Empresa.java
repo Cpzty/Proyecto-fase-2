3703f30c8502b9036ef15c5c57d2a6ee29373485
ret16339 <ret16339@uvg.edu.gt>
2016-11-17 13:14:55 -0600

    Tabla Empresa

--- Fase 2/src/Empresa.java
+++ Fase 2/src/Empresa.java
@@ -3,17 +3,21 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 
+/**En esta clase se creará la tabla "Empresa" que se utilizará en la base de datos "ReciGuate" para almacenar los datos de las Empresas
+ * @author María Mercedes Retoalza 
+ *
+ */
 public class Empresa {
    public static void main( String args[] )
      {
-       Connection c = null;
-       Statement stmt = null;
+       Connection c = null;//Se crea un objeto de tipo coneccion para conectar a la base de datos
+       Statement stmt = null;//Se crea un objeto de tipo statement, que se usara para enviarle la orden a la base de datos
        try {
-         Class.forName("org.postgresql.Driver");
+         Class.forName("org.postgresql.Driver");//Driver que se utilizará 
          c = DriverManager
             .getConnection("jdbc:postgresql://localhost:5432/ReciGuate",
-            "postgres", "root");
-         System.out.println("Opened database successfully");
+            "postgres", "root");// Instrucción que obtiene la conexion de la base de datos, incluye el nombre d ela base, el puerto , contraseña y host 
+         System.out.println("Opened database successfully");//Mensaje que indica que la base de datos se ha abierto satisfactoriamente 
 
          stmt = c.createStatement();
          String sql = "CREATE TABLE EMPRESA " +
